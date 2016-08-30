b2e4471377391133089c33d9ecc20667bc73c99d
José Muñoz Ruiz <jarmunozr@gmail.com>
2016-08-29 23:10:11 -0300

    init commit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+;(function () {
+  
+  var config = {
+    apiKey: "AIzaSyBOeBEHgqv0rzv65uj80C0KSJrwj5kz3AA",
+    authDomain: "chat-realtime-794a8.firebaseapp.com",
+    databaseURL: "https://chat-realtime-794a8.firebaseio.com",
+    storageBucket: "chat-realtime-794a8.appspot.com",
+  }
+
+  firebase.initializeApp(config)
+
+})()
