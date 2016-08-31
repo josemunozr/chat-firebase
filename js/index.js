f29d8e5c88c1a96721b73a9ccf6680893cbe53f4
José Muñoz Ruiz <jarmunozr@gmail.com>
2016-08-30 23:19:50 -0300

    - se agrega boton login con google - se agrega GoogleAuthProvider - se muestra nombre de usuario logeado

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 ;(function () {
   
-  var config = {
+  const config = {
     apiKey: "AIzaSyBOeBEHgqv0rzv65uj80C0KSJrwj5kz3AA",
     authDomain: "chat-realtime-794a8.firebaseapp.com",
     databaseURL: "https://chat-realtime-794a8.firebaseio.com",
@@ -8,5 +8,20 @@
   }
 
   firebase.initializeApp(config)
+
+  const $btnLoginGoogle =  $("#loginGoogle")
+
+  $btnLoginGoogle.on('click', () => {
+    var provider = new firebase.auth.GoogleAuthProvider()
+
+    firebase.auth().signInWithPopup(provider)
+      .then((result) => {
+        let user = result.user.displayName
+        $("#user").html(user)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  })  
 
 })()
